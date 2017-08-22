cb2e35276542cf44754c8787837b6837e2d5c587
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    msm8226-common: Add HIDL manifest
    Change-Id: I1b700b8ec9da32263c2aa180800b1df3c3ca4889

--- msm8226.mk
+++ msm8226.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Button_Jack.kl:system/usr/keylayout/Button_Jack.kl \
