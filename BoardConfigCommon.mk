fe9855059595da351ebdaa1f0d7abeb92ad24ead
LuK1337 <priv.luk@gmail.com>
2017-12-04 19:30:40 +0100

    msm8226-common: Include legacy QCOM sepolicy
    Change-Id: I9d65cecc5b0a20c4d629001f36384512ba9969a3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,5 +36,7 @@ BOARD_HARDWARE_CLASS += \
 
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
+-include device/qcom/sepolicy/legacy-sepolicy.mk
+
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
