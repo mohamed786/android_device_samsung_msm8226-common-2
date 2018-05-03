bc846f42cbdcb9b049c6fc6551ab08736924baba
HASH <hash.xda@gmail.com>
2018-05-03 20:44:58 +0530

    WIFI: merge from device tree [s3ve3g][EXP]
    Change-Id: Ib1e295d31effa9561f4cd100a9b171e73a35c126
    Signed-off-by: HASH <hash.xda@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -160,7 +160,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    hostapd_default.conf \
+    p2p_supplicant_overlay.conf \
+    wpa_supplicant_overlay.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
 # Torch
 PRODUCT_PACKAGES += \
